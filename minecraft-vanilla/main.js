ecd1efaa3a35a1c96a57f733f33ce682ec7e02bf
yishai-n-11 <yishain11@gmail.com>
2023-02-02 17:26:09 +0200

    created 3 types and assigned them randomly

--- minecraft-vanilla/main.js
+++ minecraft-vanilla/main.js
@@ -1,11 +1,18 @@
 import { config, typeToImgMap } from "./config/config.mjs";
-
+import { random } from './modules/functions.mjs';
 const grid = document.getElementById("grid");
 
 for (let index = 0; index < config.numOfRows; index++) {
   for (let j = 0; j < config.numOfCols; j++) {
     const tile = document.createElement('div');
-    tile.classList.add('tile');
+    const num = random();
+    if (num < 3) {
+      tile.classList.add('ground');
+    } else if (num < 6) {
+      tile.classList.add('tree');
+    } else {
+      tile.classList.add('sky');
+    }
     grid.append(tile);
   }
 }
